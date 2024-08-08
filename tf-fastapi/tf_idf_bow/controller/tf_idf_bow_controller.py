7e2de82ae527b71a75ec4ff669efa03b3cdf8987
Kyeongmin <124436145+2kilometer@users.noreply.github.com>
2024-08-08 15:09:03 +0900

    feat: [이경민] 사용자의 질문 수신 [TFFA1]

--- tf-fastapi/tf_idf_bow/controller/tf_idf_bow_controller.py
+++ tf-fastapi/tf_idf_bow/controller/tf_idf_bow_controller.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 
+from tf_idf_bow.controller.request_form.tf_idf_bow_request_form import TfIdfBowRequestForm
 from tf_idf_bow.service.tf_idf_bow_service_impl import TfIdfBowServiceImpl
 
 tfIdfBowRouter = APIRouter()
@@ -9,7 +10,9 @@ async def injectTfIdfBowService() -> TfIdfBowServiceImpl:
     return TfIdfBowServiceImpl()
 
 @tfIdfBowRouter.post("/find-similar-answer")
-async def findSimilarAnswer(tfIdfBowService: TfIdfBowServiceImpl =
+async def findSimilarAnswer(tfIdfBowRequestForm: TfIdfBowRequestForm,
+                            tfIdfBowService: TfIdfBowServiceImpl =
                             Depends(injectTfIdfBowService)):
 
     print(f"controller -> findSimilarAnswer(): tfIdfBowRequestForm")
+    print(f"controller -> RequestForm: {tfIdfBowRequestForm.userSendQuestion}")
