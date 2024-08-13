800e7450d1ed90d9d2d754efe37b819d68ae3a32
Kyeongmin <124436145+2kilometer@users.noreply.github.com>
2024-08-13 15:23:23 +0900

    refactor: [이경민] 백터화 코드 버전 추가 [TFFA-2]

--- tf-fastapi/app/assets/extract_answers_vector.py
+++ tf-fastapi/app/assets/extract_answers_vector.py
@@ -1,31 +1,44 @@
-import pickle
+import pickle, time
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-answerDf = pd.read_pickle("answers.pickle")
-
-introAnswerList = answerDf['answer_intro'].tolist()
-totalAnswerList = (answerDf['answer_intro'] + " " +
-                   answerDf['answer_body'] + " " +
-                   answerDf['answer_conclusion']).tolist()
-
 def answerVectorization(answerlist):
+    print("백터화 시작")
     countVectorizer = CountVectorizer()
     countMatrix = countVectorizer.fit_transform(answerlist)
+    print("백터화 종료")
 
     return countVectorizer, countMatrix, answerlist
 
 def saveAsPickle(filename, objects):
-    for obj in objects:
-        with open(f"{filename}.pickle", "wb") as file:
+    with open(f"{filename}.pickle", "wb") as file:
+        for obj in objects:
             pickle.dump(obj, file)
 
-saveAsPickle(
-    "introAnswerVectorization",
-    answerVectorization(introAnswerList[:2]))
 
-# saveAsPickle(
-#     "totalAnswerVectorization",
-#     answerVectorization(totalAnswerList))
+if __name__ == '__main__':
+    answerDf = pd.read_pickle("answers.pickle")
 
+    start_time = time.time()
+
+    introAnswerList = answerDf['answer_intro'].tolist()
+    saveAsPickle(
+        "introAnswerVectorization",
+        answerVectorization(introAnswerList))
+
+    end_time = time.time()
+    print(f"introAnswerVectorization 소요시간 : {end_time - start_time} 초", end="\n")
+
+
+    start_time = time.time()
+
+    totalAnswerList = (answerDf['answer_intro'] + " " +
+                       answerDf['answer_body'] + " " +
+                       answerDf['answer_conclusion']).tolist()
+    saveAsPickle(
+        "totalAnswerVectorization",
+        answerVectorization(totalAnswerList))
+
+    end_time = time.time()
+    print(f"totalAnswerVectorization 소요시간 : {end_time - start_time} 초")
